edf81bc734e44fe08bb22c254d1f506de73e46b4
Hafiizh Ghulam <ghulam@jejeharapan.com>
2025-09-29 17:33:53 +0700

    Update 404 status

--- routes/api.ts
+++ routes/api.ts
@@ -3,7 +3,7 @@ import hello from "@/routes/hello";
 import Router from "@/utils/Router";
 
 export default {
-    "/*": new Response().setMessage("Not Found").send(),
+    "/*": new Response().setMessage("Not Found").setStatus(404).send(),
 
     ...new Router().prefix("api").group({
         ...hello
