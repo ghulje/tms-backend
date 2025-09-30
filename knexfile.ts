86df98a20ca295d63326859ba7eb720a9c20418c
Hafiizh Ghulam <ghulam@jejeharapan.com>
2025-09-30 18:49:34 +0700

    Force Update

--- knexfile.ts
+++ knexfile.ts
@@ -3,11 +3,11 @@ import type {Knex} from "knex";
 const config: Knex.Config = {
     client: "pg",
     connection: {
-        host: "127.0.0.1",
-        port: 5432,
-        user: "postgres",
-        password: "jeje",
-        database: "bun"
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_DATABASE
     },
     migrations: {
         extension: "ts",
